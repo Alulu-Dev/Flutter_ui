4ad99b10113b8b78d9fa40a62a4bea7e5612264e
zekariyas-alemu <zekialemu3@gmail.com>
2022-04-23 19:03:55 +0300

    new

---
+++ receipt_management/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
+import 'package:receipt_management/auth/login/bloc/login_bloc.dart';
+import 'package:receipt_management/auth/profile/bloc/profile_bloc.dart';
+import 'package:receipt_management/auth/register/bloc/register_bloc.dart';
+import 'package:receipt_management/budget/bloc/bloc.dart';
+import 'package:receipt_management/expense/bloc/bloc.dart';
+import 'package:receipt_management/home/comparison/bloc/bloc.dart';
+import 'package:receipt_management/home/prediction/bloc/bloc.dart';
+import 'package:receipt_management/home/upload/bloc/uploading_bloc.dart';
+import 'package:receipt_management/home/wallet/bloc/bloc.dart';
+import 'package:receipt_management/requests/bloc/request_bloc.dart';
+
+import 'config/routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    FlutterStatusbarcolor.setStatusBarColor(Colors.white);
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AuthBloc()),
+        BlocProvider(create: (context) => RegistrationBloc()),
+        BlocProvider(create: (context) => ProfileBloc()),
+        BlocProvider(create: (context) => WalletBloc()),
+        BlocProvider(create: (context) => ReceiptBloc()),
+        BlocProvider(create: (context) => RequestBloc()),
+        BlocProvider(create: (context) => UploadBloc()),
+        BlocProvider(create: (context) => ComparisonBloc()),
+        BlocProvider(create: (context) => ComparisonDetailBloc()),
+        BlocProvider(create: (context) => ComparisonCreateBloc()),
+        BlocProvider(create: (context) => CategorySummaryBloc()),
+        BlocProvider(create: (context) => SingleCategorySummaryBloc()),
+        BlocProvider(create: (context) => BudgetBloc()),
+        BlocProvider(create: (context) => NewBudgetBloc()),
+        BlocProvider(create: (context) => UserRequestBloc()),
+        BlocProvider(create: (context) => PredictionBloc()),
+        BlocProvider(create: (context) => ReceiptsSummaryBloc()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Receipt Management',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        // home: HomePage(),
+        initialRoute: '/login',
+        onGenerateRoute: RouteGenerator.generateRoute,
+      ),
+    );
+  }
+}
