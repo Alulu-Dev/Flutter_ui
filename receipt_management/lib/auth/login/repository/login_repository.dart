a8d0d79ca105282544dd9b04cbe0da2ecc903f06
zekariyas-alemu <zekialemu3@gmail.com>
2022-04-26 00:45:38 +0300

    login_screen repos

--- receipt_management/lib/auth/login/repository/login_repository.dart
+++ receipt_management/lib/auth/login/repository/login_repository.dart
@@ -1,1 +1,13 @@
+import 'package:http/http.dart' as http;
+import 'package:receipt_management/auth/login/data_providers/login_data_provider.dart';
 
+class LoginRepository {
+  final LoginDataProvider loginDataProvider =
+      LoginDataProvider(httpClient: http.Client());
+
+  // LoginRepository();
+
+  Future<String> loginRoute(String email, String pass) async {
+    return await loginDataProvider.loginRoute(email, pass);
+  }
+}
